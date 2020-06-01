2823c8b1ef913a9e5d48fb8306c48993f5bb8b29
Matheus Caetano <caetanomatheus876@gmail.com>
2020-05-31 21:25:02 -0300

    Tranformação do select de certificado em um componente

--- src/App.vue
+++ src/App.vue
@@ -8,26 +8,8 @@
         alt="soluti"
       />
 
-      <div class="form-group">
-        <label for="certificate_type">Escolha uma das opções abaixo</label>
-        <select
-          class="form-control"
-          id="certificate_type"
-          v-model="certificateType"
-        >
-          <option value="">-- Selecione --</option>
-          <option value="certificate_pf">Certificado Pessoa Física</option>
-          <option value="certificate_pj">Certificado Pessoa Jurídica</option>
-        </select>
-      </div>
-
-      <div class="form-group">
-        <button class="btn btn-success btn-sm" @click="loadQuestions()">
-          Carregar Perguntas
-        </button>
-        <button class="btn btn-danger btn-sm ml-3">
-          Limpar Lista
-        </button>
+      <div>
+        <certificate-select />
       </div>
 
       <div>
@@ -38,10 +20,12 @@
 </template>
 
 <script>
+import CertificateSelect from './components/CertificateSelect.vue'
 import QuestionsTable from './components/QuestionsTable.vue'
 
 export default {
   components: {
+    CertificateSelect,
     QuestionsTable,
   },
 
---
+++ src/components/CertificateSelect.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div class="form-group">
+      <label for="certificate_type">Escolha uma das opções abaixo</label>
+      <select
+        class="form-control"
+        id="certificate_type"
+        v-model="certificateType"
+      >
+        <option value="">-- Selecione --</option>
+        <option value="certificate_pf">Certificado Pessoa Física</option>
+        <option value="certificate_pj">Certificado Pessoa Jurídica</option>
+      </select>
+    </div>
+
+    <div class="form-group">
+      <button class="btn btn-success btn-sm" @click="loadQuestions()">
+        Carregar Perguntas
+      </button>
+      <button class="btn btn-danger btn-sm ml-3">
+        Limpar Lista
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    certificateType: '',
+  }),
+
+  methods: {
+    loadQuestions() {
+      if (this.certificateType === 'certificate_pf') {
+        this.$store.dispatch('loadPfQuestions')
+      } else if (this.certificateType === 'certificate_pj') {
+        this.$store.dispatch('loadPjQuestions')
+      } else {
+        window.Swal.toast().fire(
+          {
+            icon: 'error',
+            title: 'Por favor, selecione o tipo de pessoa antes de carregar as perguntas',
+          },
+        )
+      }
+    },
+  },
+}
+</script>
+
+<style></style>
